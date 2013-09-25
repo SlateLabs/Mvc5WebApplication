424e977c34cd7a9e7c94841cb0bf01a1abed19b1
Jim Lamb <jim@jflamb.com>
2013-09-25 11:00:44 -0400

    Initial commit

---
+++ MVC5EntityFrameworkDataAccess/EFDataService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVC5DataServiceInterface;
+
+namespace MVC5EntityFrameworkDataAccess
+{
+   
+    public class EFDataService : IDataService, IDisposable
+    {
+
+        MVC5Database _connection;
+
+        public MVC5Database dbConnection
+        {
+            get { return _connection; }
+        }
+
+
+        public void CommitTransaction(Boolean closeSession)
+        {
+            dbConnection.SaveChanges();
+        }
+
+        public void RollbackTransaction(Boolean closeSession)
+        {
+           
+        }
+        
+        public void Save(object entity) { }
+        public void CreateSession() { _connection = new MVC5Database(); }
+        public void BeginTransaction() { }
+
+        public void CloseSession() { }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+                _connection.Dispose();
+        }
+    }
+}
---
+++ MVC5EntityFrameworkDataAccess/MVC5Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using MVC5Models;
+using MVC5DataServiceInterface;
+
+namespace MVC5EntityFrameworkDataAccess
+{
+    public class MVC5Database : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<PaymentType> PaymentTypes { get; set; }
+     
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>().ToTable("dbo.Customers");
+            modelBuilder.Entity<PaymentType>().ToTable("dbo.PaymentTypes");    
+        }
+    }
+
+}
